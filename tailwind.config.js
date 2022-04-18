263831db216843a8642f31f48ff730a6517456e3
joaomira24 <72982787+joaomira24@users.noreply.github.com>
2022-04-18 19:54:36 +0200

    First draft

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	content: ["./src/*.html"],
+	theme: {
+		extend: {},
+		colors: {
+			surfCrest: "#BEDBBB",
+		},
+		fontFamily: {
+			Cutive: ["Cutive Mono", "monospace"],
+			IBM: ["IBM Plex Mono", "monospace"],
+		},
+	},
+	plugins: [],
+};
