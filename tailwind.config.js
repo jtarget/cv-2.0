6152a50aa648d104b71fd9352fe743b16c6b3697
joaomira24 <72982787+joaomira24@users.noreply.github.com>
2022-04-22 01:06:35 +0200

    finished porting elements from previous version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,30 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-	content: ["./src/*.html"],
+	content: ["./src/*.html", "./node_modules/tw-elements/dist/js/**/*.js"],
 	theme: {
-		extend: {},
-		colors: {
-			surfCrest: "#BEDBBB",
+		extend: {
+			keyframes: {
+				wiggle: {
+					"0%, 100%": { transform: "rotate(-3deg)" },
+					"50%": { transform: "rotate(3deg)" },
+				},
+			},
+			colors: {
+				surfCrest: "#BEDBBB",
+			},
+			boxShadow: {
+				classic: "inset 0 -0.6em 0 -0.35em rgba(0, 0, 0, 0.17)",
+			},
+			animation: {
+				wiggle: "wiggle 1s ease-in-out infinite",
+				spinslow: "spin 3s linear infinite",
+			},
 		},
 		fontFamily: {
 			Cutive: ["Cutive Mono", "monospace"],
 			IBM: ["IBM Plex Mono", "monospace"],
 		},
 	},
-	plugins: [],
+	plugins: [require("tw-elements/dist/plugin")],
 };
