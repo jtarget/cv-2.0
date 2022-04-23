46195af538329a8fc3f4e34d4b08a730b9330e3e
joaomira24 <72982787+joaomira24@users.noreply.github.com>
2022-04-23 16:50:36 +0200

    updated intro + moved folder + carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-	content: ["./*.html", "./node_modules/tw-elements/dist/js/**/*.js"],
+	content: ["./*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
 	theme: {
 		extend: {
 			keyframes: {
